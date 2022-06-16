5ff82edf0cc4b9d721895fd1aab381a129c1c303
lscitl <lscitl@naver.com>
2022-06-16 23:57:53 +0900

    folder / char added

--- src/minishell_aster.c
+++ src/minishell_aster.c
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 22:24:10 by seseo             #+#    #+#             */
-/*   Updated: 2022/06/16 15:10:03 by seseo            ###   ########.fr       */
+/*   Updated: 2022/06/16 23:54:12 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,12 @@ static void	asterisk_sub2(t_dir_list **dir_list,
 	if ((ad_flag && ast_strs[0] == NULL) || (ad_flag & 4 && !ast_strs[0][0]))
 	{
 		if (d_name[0] != '.')
-			ft_lstadd_back(dir_list, ft_lstnew(ft_strdup(d_name)));
+		{
+			if (ad_flag & 4)
+				ft_lstadd_back(dir_list, ft_lstnew(ft_strjoin(d_name, "/")));
+			else
+				ft_lstadd_back(dir_list, ft_lstnew(ft_strdup(d_name)));
+		}
 	}
 	else if ((!(ad_flag & 1)
 			&& !ft_strncmp(d_name, ast_strs[0], ft_strlen(ast_strs[0])))
@@ -127,12 +132,15 @@ static void	asterisk_add_files(t_dir_list **dir_list,
 			tmp += ft_strlen(ast_strs[i]);
 		i++;
 	}
-	if (tmp)
+	if (tmp == NULL)
+		return ;
+	if ((!(ad_flag & 2) && !ft_strncmp(d_name + ft_strlen(d_name)
+				- ft_strlen(ast_strs[i - 1]), ast_strs[i - 1], -1))
+		|| ad_flag & 2)
 	{
-		if ((!(ad_flag & 2) && !ft_strncmp(d_name + ft_strlen(d_name)
-					- ft_strlen(ast_strs[i - 1]), ast_strs[i - 1], -1)))
-			ft_lstadd_back(dir_list, ft_lstnew(ft_strdup(d_name)));
-		else if (ad_flag & 2)
+		if (ad_flag & 4)
+			ft_lstadd_back(dir_list, ft_lstnew(ft_strjoin(d_name, "/")));
+		else
 			ft_lstadd_back(dir_list, ft_lstnew(ft_strdup(d_name)));
 	}
 }
