0b7a0d847e8a2c64596ccc729683a1296297e9ce
khk811 <hyunkyungkim811@gmail.com>
2022-06-22 13:58:09 +0900

    ad make_cmd_str

--- src/minishell_exec_do_and_or.c
+++ src/minishell_exec_do_and_or.c
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 22:46:22 by seseo             #+#    #+#             */
-/*   Updated: 2022/06/21 23:27:22 by seseo            ###   ########.fr       */
+/*   Updated: 2022/06/22 13:57:50 by seseo            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,25 +71,26 @@ int	do_builtin(t_info *info, char **cmd)
 		return (b_export(cmd, info));
 }
 
-// char	**make_cmd_strs(t_info *info, t_token *tokens)
-// {
-// 	char	**cmd;
-// 	char	**ast_strs;
-// 	char	*tmp;
-// 	int		i;
+char	**make_cmd_strs(t_token *token)
+{
+	char	**ret;
+	t_token	*cur;
+	t_token	*dir;
+	char	*tmp;
 
-// 	cmd = tokens_to_str(tokens);
-// 	i = 0;
-// 	while (cmd[i])
-// 	{
-// 		tmp = cmd[i];
-// 		cmd[i] = expand_string_elem(info, cmd[i]);
-// 		free(tmp);
-// 		tmp = cmd[i];
-// 		cmd = merge_strs(cmd, asterisk_expand(info, cmd[i]), i);
-
-// 	}
-// }
+	cur = token;
+	dir = NULL;
+	while (cur)
+	{
+		tmp = expand_string_elem(info, cur->content);
+		token_add_back(&dir, asterisk_expand(info, tmp));
+		free(tmp);
+		cur = cur->next;
+	}
+	ret = tokens_to_str(dir);
+	token_del(dir);
+	return (ret);
+}
 
 int	do_cmd(t_info *info, t_b_node *root)
 {
