350fd9b5c3d8546504c83cec0c40414e542abed9
Seungduk Seo <seseo@student.42seoul.kr>
2022-06-27 01:21:12 +0900

    handling directory error fuction added. norm check ok.

--- src/minishell_exec_do_cmd_child.c
+++ src/minishell_exec_do_cmd_child.c
@@ -6,11 +6,14 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:10:49 by seseo             #+#    #+#             */
-/*   Updated: 2022/06/26 18:34:35 by seseo            ###   ########.fr       */
+/*   Updated: 2022/06/27 00:45:55 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	do_cmd_with_actual_path(t_info *info);
+static int	do_cmd_with_env_path(t_info *info, t_env_list *path_node);
 
 int	do_cmd(t_info *info, t_b_node *root)
 {
@@ -40,42 +43,67 @@ int	do_cmd(t_info *info, t_b_node *root)
 
 int	do_cmd_child(t_info *info, t_b_node *root)
 {
-	char	**path;
-	char	**env;
-	int		i;
-	int		redir_status;
+	t_env_list	*path_node;
+	int			redir_status;
 
 	info->plv++;
 	redir_status = apply_redir(info, root);
-	// signal(SIGINT, &sig_exec_child);
-	// signal(SIGQUIT, &sig_exec_child);
 	if (redir_status)
 		return (redir_status);
 	if (info->cmd[0] == NULL)
-		exit (EXIT_SUCCESS);
-	path = ft_split(find_key(info->env_list, "PATH")->value, ':');
-	if (path)
+		return (EXIT_SUCCESS);
+	if (strchr(info->cmd[0], '/'))
+		return (do_cmd_with_actual_path(info));
+	path_node = find_key(info->env_list, "PATH");
+	if (path_node)
+		return (do_cmd_with_env_path(info, path_node));
+	print_err_msg(info->cmd[0], "No such file or directory");
+	return (ERROR_EXIT);
+}
+
+static int	do_cmd_with_actual_path(t_info *info)
+{
+	struct stat	statbuf;
+	char		**env;
+
+	env = get_env_strs(info);
+	execve(info->cmd[0], info->cmd, env);
+	if (errno == EACCES)
 	{
-		i = 0;
-		while (path[i])
-		{
-			path[i] = ft_strjoin(path[i], "/");
-			i++;
-		}
-		env = get_env_strs(info);
-		i = 0;
-		if (strchr(info->cmd[0], '/'))
-			execve(info->cmd[0], info->cmd, env);
+		stat(info->cmd[0], &statbuf);
+		if (S_ISDIR(statbuf.st_mode))
+			print_err_msg(info->cmd[0], "is a directory");
 		else
-		{
-			while (path[i] && execve(ft_strjoin(path[i++], info->cmd[0]), info->cmd, env) && errno == ENOENT)
-				;
-		}
-		print_err_msg(info->cmd[0], strerror(errno));
-		return (127);
+			print_err_msg(info->cmd[0], strerror(errno));
+		return (ERROR_PERMISSION);
 	}
 	print_err_msg(info->cmd[0], strerror(errno));
-	return (127);
+	return (ERROR_EXIT);
+}
+
+static int	do_cmd_with_env_path(t_info *info, t_env_list *path_node)
+{
+	char	**env;
+	char	**path;
+	int		i;
+
+	env = get_env_strs(info);
+	path = ft_split(path_node->value, ':');
+	i = 0;
+	while (path[i])
+	{
+		path[i] = ft_strjoin(path[i], "/");
+		i++;
+	}
+	i = 0;
+	while (path[i] && execve(ft_strjoin(path[i], info->cmd[0]), info->cmd, env)
+		&& errno == ENOENT)
+		i++;
+	if (errno == ENOENT)
+		print_err_msg(info->cmd[0], "command not found");
+	else
+		print_err_msg(ft_strjoin(path[i], info->cmd[0]), strerror(errno));
+	return (ERROR_EXIT);
 }
 
 int	execute_bt_node(t_info *info, t_b_node *root)
