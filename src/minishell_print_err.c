474c91da7123ca3dff2cdc6495933b3ff42840c8
Seungduk Seo <seseo@student.42seoul.kr>
2022-06-27 01:23:30 +0900

    printing err msg with out command string function added.

--- src/minishell_print_err.c
+++ src/minishell_print_err.c
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 16:20:28 by seseo             #+#    #+#             */
-/*   Updated: 2022/06/26 18:49:49 by seseo            ###   ########.fr       */
+/*   Updated: 2022/06/27 00:28:41 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,23 @@ int	print_err_msg(char *cmd, char *errmsg)
 	add_str(buf, SHELL_NAME);
 	add_str(buf, ": ");
 	add_str(buf, cmd);
+	add_str(buf, ": ");
+	add_str(buf, errmsg);
+	add_char(buf, '\n');
+	err_msg = put_str(buf);
+	ft_putstr_fd(err_msg, 2);
+	del_buf(buf);
+	free(err_msg);
+	return (FALSE);
+}
+
+int	print_err_msg_no_cmd(char *errmsg)
+{
+	t_buffer	*buf;
+	char		*err_msg;
+
+	buf = create_buf();
+	add_str(buf, SHELL_NAME);
 	add_str(buf, ": ");
 	add_str(buf, errmsg);
 	add_char(buf, '\n');
