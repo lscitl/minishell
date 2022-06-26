13542d5834e632c5554a806a99fd5a14ee008016
Seungduk Seo <seseo@student.42seoul.kr>
2022-06-27 01:20:15 +0900

    usless fuctions are deleted

--- src/minishell_bintree.c
+++ src/minishell_bintree.c
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 16:07:31 by seseo             #+#    #+#             */
-/*   Updated: 2022/06/26 17:48:45 by seseo            ###   ########.fr       */
+/*   Updated: 2022/06/27 00:28:45 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,4 @@ void	del_btree(t_b_node *node)
 	token_del(node->tokens);
 	token_del(node->redir);
 	free(node);
-}
-
-void	inorder_btree(t_b_node *p_node)
-{
-// for debug
-	if (p_node == NULL)
-		return ;
-	inorder_btree(p_node->left);
-	fprintf(stderr, "CMD_TYPE: %d\n", p_node->type);
-	fprintf(stderr, "token: ");
-	print_content(p_node->tokens);
-	fprintf(stderr, "redir: ");
-	print_content(p_node->redir);
-	fprintf(stderr, "\n");
-	inorder_btree(p_node->right);
-}
-
-void	print_content(t_token *tokens)
-{
-	t_token	*tmp;
-
-// for debug
-	tmp = tokens;
-	while (tmp)
-	{
-		fprintf(stderr, "%s ", tmp->content);
-		tmp = tmp->next;
-	}
-	fprintf(stderr, "\n");
 }
